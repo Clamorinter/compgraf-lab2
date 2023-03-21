dd032dd40fbf10991151e08bb3a85225744e7179
Clamorinter <MRakhilov@gmail.com>
2023-03-21 19:17:43 +0300

    Worked on Main.cpp template for work

--- main.cpp
+++ main.cpp
@@ -1,26 +1,85 @@
 #include <Windows.h>
 #include "figure.h"
 #include <iostream>
+
+#define X_SCREEN 1200
+#define Y_SCREEN 800
+
+bool mouseprocessing(int& x, int& y, bool& clicked);
+
 int main(void)
 {
-	void* bitmap = nullptr;
 	HWND hwnd;
-	initwindow(600, 600, "Lines");
+	initwindow(X_SCREEN, Y_SCREEN, "Lines");
+	setbkcolor(15);
+
+	//hwnd = GetConsoleWindow();
+	//ShowWindow(hwnd, SW_SHOW);
+
 	hwnd = FindWindowA(0, "Lines");
-	bitmap = (void*)malloc(imagesize(0, 0, 600, 600));
+	void* bitmap = (void*)malloc(imagesize(0, 0, X_SCREEN, Y_SCREEN));
 	setvisualpage(0);
+	
+	dynarr<Figure**> figures;
+	int x, y;
+
+	bool clicked = false;
+
+	char c;
+
+	setcolor(0);
+
 	while (1)
 	{
-		Sleep(1);
+		delay(17);
+
+		if (mouseprocessing(x, y, clicked)) //working probably how I need
+		{
+			std::cout << x << " " << y << std::endl; 
+		}
+
+		if (kbhit())
+		{
+			c = (char)getch();
+			std::cout << c << std::endl;
+		}
+
 		setactivepage(1);
-		std::cout << "Page 1" << std::endl;
-		line(100, 100, 400, 400);
-		line(100, 200, 200, 100);
-		line(400, 100, 100, 400);
-		getimage(0, 0, 600, 600, bitmap);
+
+		bgiout << mousex() << " " << mousey();
+		outstreamxy(10, 10);
+
+		getimage(0, 0, X_SCREEN, Y_SCREEN, bitmap);
 		clearviewport();
 		setactivepage(0);
-		std::cout << "Page 0" << std::endl;
+
 		putimage(0, 0, bitmap, COPY_PUT);
 	}
+}
+
+bool mouseprocessing(int& x, int& y, bool& clicked)
+{
+	int start_x = x;
+	int start_y = y;
+	if (ismouseclick(WM_LBUTTONDOWN))
+	{
+		getmouseclick(WM_LBUTTONDOWN, x, y);
+		clicked = true;
+		return true;
+	}
+	if (ismouseclick(WM_LBUTTONUP))
+	{
+		clearmouseclick(WM_LBUTTONUP);
+		clicked = false;
+		return false;
+	}
+	if (ismouseclick(WM_MOUSEMOVE) && clicked)
+	{
+		getmouseclick(WM_MOUSEMOVE, x, y);
+		if (start_x != x && start_y != y)
+		{
+			return true;
+		}
+	}
+	return false;
 }
