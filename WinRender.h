7c8ed5dce3d94d7d608c954ea8fa296ad2eae3b4
Clamorinter <MRakhilov@gmail.com>
2023-03-21 20:08:50 +0300

    Added renderer header

---
+++ WinRender.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "figure.h"
+
+class WinRender
+{
+public:
+	WinRender(int x, int y);
+	~WinRender();
+
+	//do functions
+	void doAMouse();
+	void doAKey();
+	void doAChange();
+
+	//mouse functions
+	void createmode(int x, int y);
+	void movemode(int x, int y);
+	void dragmode(int x, int y);
+	void choosing(int x, int y);
+	bool mouseProcessing();
+
+	//key functions
+	void movekey(char key);
+	void createkey(char key);
+	void choosekey(char key);
+	void chooseallkey();
+	void rotatekey(char key);
+	void zoomkey(char key);
+	void deletekey();
+	void exitkey();
+
+	//menu function
+	void menu();
+	//flag functions
+	bool isExit();
+
+private:
+
+	dynarr<Figure**> figures;
+	int choosed;
+	int x, y;
+	int numOfElement;
+	char c;
+
+	bool clickflag = false;
+	bool exitflag = false;
+	bool createflag = false;
+	bool moveflag = false;
+	bool changeflag = false;
+	bool chooseflag = false;
+};
